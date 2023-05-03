ef03a2fd8d2e270d6c3225daa2aefef546275ff8
김경호 <>
2023-05-04 00:05:28 +0900

    toolbar include dataBinding 적용 중( 해결 x)

--- app/src/main/java/com/myosa/triplan/ui/fragment/TripHomeFragment.kt
+++ app/src/main/java/com/myosa/triplan/ui/fragment/TripHomeFragment.kt
@@ -1,20 +1,45 @@
 package com.myosa.triplan.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.myosa.triplan.BaseFragment
 import com.myosa.triplan.R
 import com.myosa.triplan.databinding.FragmentTripHomeBinding
 
 
 class TripHomeFragment :BaseFragment<FragmentTripHomeBinding>(R.layout.fragment_trip_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun initView() {
         binding.apply {
+            includedToolbar.tbTrip.setNavigationOnClickListener{
+                findNavController().popBackStack()
+            }
+            includedToolbar.tbTrip.setOnMenuItemClickListener{when(it.itemId){
+                R.id.edit_trip -> {
+                    Toast.makeText(this@TripHomeFragment.context, "수정", Toast.LENGTH_SHORT).show()
+                    true}
+                R.id.delete_trip -> {
+                    Toast.makeText(this@TripHomeFragment.context, "삭제", Toast.LENGTH_SHORT).show()
+                    true}
+                else -> false
+            }
+            }
+
 
         }
+
     }
+
 
 }
