af0d7acc8ad4f27c863f6b704c0b465a31ab0678
see-ho <siki7878@gmail.com>
2023-08-22 13:31:38 +0900

    base dialog 기반

--- app/src/main/java/com/capstone/triplan/di/CommonUtil.kt
+++ app/src/main/java/com/capstone/triplan/di/CommonUtil.kt
@@ -1,7 +1,10 @@
 package com.capstone.triplan.di
 
+import android.app.Activity
 import android.content.Context
+import android.os.Build
 import android.util.DisplayMetrics
+import android.view.WindowInsets
 import android.view.WindowManager
 import com.capstone.triplan.R
 import java.net.MalformedURLException
@@ -40,6 +43,20 @@ object CommonUtil {
 
     fun createHyperlink(url: String): String {
         return "<a href=\"$url\">$url</a>"
+    }
+
+    fun getScreenWidth(activity: Activity): Int {
+        val windowManager = activity.applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            val windowMetrics = windowManager.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.width() - insets.left - insets.right
+        }else{
+            val displayMetrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.widthPixels
+        }
     }
 
 }
